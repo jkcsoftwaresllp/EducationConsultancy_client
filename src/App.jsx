c6e9c708e82c463d9e1c531ad52564aafe6d0c74
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-06-19 12:04:10 +0530

    Refactor App component structure and clean up Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import TopBar from "./components/topBar/topBar";
+import Navbar from "./components/navBar/navBar";
+import { useState } from "react";
 
 function App() {
+  const [courseUrl, setCourseUrl] = useState("/");
+
   return (
-    // <Router>  <--- Creating error
+    <>
+      <TopBar
+        address="5A, Bhagwati Sharan Enclave, Boring Road, Patna-1"
+        phone="+91-097080 44044"
+        email="info@admissionexperts.co.in"
+      />
+      <Navbar
+        onCourseClick={(value) => {
+          setCourseUrl(value);
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    // </Router>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,5 @@
-import TopBar from "../components/topBar/topBar";
-import Navbar from "../components/navBar/navBar";
-import { useState } from "react";
-// import {HoverTile} from '../components/courseDetails/HoverTile';
-
 function Home() {
-
-  const [courseUrl, setCourseUrl] = useState("/")
-
-  return (
-    <>
-      <TopBar
-        address="5A, Bhagwati Sharan Enclave, Boring Road, Patna-1"
-        phone="+91-097080 44044"
-        email="info@admissionexperts.co.in"
-      />
-      <Navbar
-        onCourseClick={(value) => {
-          console.log(value);
-        }}
-      />
-    </>
-  );
+  return <>Home</>;
 }
 
 export default Home;
