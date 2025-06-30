0a80793ea69afdad8e23da40f750601c56dd8de9
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-06-30 13:47:52 +0530

    Refactor App component layout for improved readability and structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TopBar from "./components/topBar/topBar";
 import Navbar from "./components/navBar/navBar";
 import { useEffect, useState } from "react";
 import { BTech } from "./components/coursesRoutes/BTech";
-import {MTech} from "./components/coursesRoutes/MTech";
+import { MTech } from "./components/coursesRoutes/MTech";
 import Phd from "./components/coursesRoutes/Phd";
 import DiplomaCourses from "./components/coursesRoutes/DiplomaCourses";
 import Mbbs from "./components/coursesRoutes/Mbbs";
@@ -16,48 +16,57 @@ import Footer from "./components/footer/footer";
 
 function App() {
   const [courseUrl, setCourseUrl] = useState("");
-  console.log(courseUrl)
-  const navigate = useNavigate()
-  useEffect(()=>{
-    navigate(courseUrl)
-  },[courseUrl])
+  console.log(courseUrl);
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(courseUrl);
+  }, [courseUrl]);
 
   const toKebabCase = (str) => {
-  return str
-    .toLowerCase()
-    .replace(/\./g, '')           // remove all dots
-    .replace(/[\/\s]+/g, '-')     // replace slashes and spaces with hyphen
-    .replace(/^-+|-+$/g, '');     // remove leading/trailing hyphens
-};
+    return str
+      .toLowerCase()
+      .replace(/\./g, "") // remove all dots
+      .replace(/[\/\s]+/g, "-") // replace slashes and spaces with hyphen
+      .replace(/^-+|-+$/g, ""); // remove leading/trailing hyphens
+  };
   return (
     <>
-      <TopBar
-        address="511, 5 Floor,Ashiana tower, Exhibition Rd, near HDFC BANK LTD, Ali Nagar Colony, Salimpur Ahra, Patna, Bihar 800001"
-        phone="+91 93043 65563"
-        email="info@thecareeezone.co.in"
-      />
-      <Navbar
-        onCourseClick={(value) => {
-          const kebabValue = toKebabCase(value);
-          (kebabValue === "home") ? setCourseUrl(`/`) : setCourseUrl(`/${kebabValue}`);
-        }}
-      />
-      {/* <button>Register</button> */}
-      <EnquiryForm />
-      
-     
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses-be-btech" element={<BTech/>} />
-        <Route path="/courses-me-mtech" element={<MTech/>} />
-        <Route path="/courses-phd" element={<Phd/>} />
-        <Route path="/courses-diploma-courses" element={<DiplomaCourses/>} />
-        <Route path="/courses-mbbs" element={<Mbbs/>} />
-        <Route path="/courses-md" element={<Md/>} />    
-        <Route path="/about-us" element={<AboutUs />} />    
-        <Route path="/contact-us" element={<ContactUs />} />    
-      </Routes>
-      <Footer/>
+      <div>
+        <div>
+          <TopBar
+            address="511, 5 Floor,Ashiana tower, Exhibition Rd, near HDFC BANK LTD, Ali Nagar Colony, Salimpur Ahra, Patna, Bihar 800001"
+            phone="+91 93043 65563"
+            email="info@thecareeezone.co.in"
+          />
+          <Navbar
+            onCourseClick={(value) => {
+              const kebabValue = toKebabCase(value);
+              kebabValue === "home"
+                ? setCourseUrl(`/`)
+                : setCourseUrl(`/${kebabValue}`);
+            }}
+          />
+        </div>
+        {/* <button>Register</button> */}
+        <EnquiryForm />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses-be-btech" element={<BTech />} />
+            <Route path="/courses-me-mtech" element={<MTech />} />
+            <Route path="/courses-phd" element={<Phd />} />
+            <Route
+              path="/courses-diploma-courses"
+              element={<DiplomaCourses />}
+            />
+            <Route path="/courses-mbbs" element={<Mbbs />} />
+            <Route path="/courses-md" element={<Md />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
